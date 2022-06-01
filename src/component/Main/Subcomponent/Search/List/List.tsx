c721c359705bf2cd64e2af5a9a10d9fe53631ab8
Artur Zaczek <artur200212@interia.pl>
2022-06-01 07:09:10 +0200

    added inon to table

--- src/component/Main/Subcomponent/Search/List/List.tsx
+++ src/component/Main/Subcomponent/Search/List/List.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './List.scss'
 
+import { ReactComponent as Thumb } from '../../../../../img/thumbsUp.svg'
+import { ReactComponent as Star } from '../../../../../img/star.svg'
 const List = () => {
 
     return (
@@ -21,64 +23,64 @@ const List = () => {
                     <td>Title1</td>
                     <td>Author1</td>
                     <td><button>Read</button></td>
-                    <td><img src="/" alt="up" /><img src="" alt="down" /></td>
-                    <td><img src="/" alt="star" /></td>
+                    <td><Thumb className='img img__up img__up--active' /><Thumb className='img img__down img__down--active' /></td>
+                    <td><Star className='star  ' /></td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td>Title2</td>
                     <td>Author3</td>
                     <td><button>Read</button></td>
-                    <td><img src="/" alt="up" /><img src="" alt="down" /></td>
-                    <td><img src="/" alt="star" /></td>
+                    <td><Thumb className='img img__up img__up--active' /><Thumb className='img img__down img__down--active' /></td>
+                    <td><Star className='star  ' /></td>
                 </tr>
                 <tr>
                     <td>1</td>
                     <td>Title1</td>
                     <td>Author1</td>
                     <td><button>Read</button></td>
-                    <td><img src="/" alt="up" /><img src="" alt="down" /></td>
-                    <td><img src="/" alt="star" /></td>
+                    <td><Thumb className='img img__up img__up--active' /><Thumb className='img img__down img__down--active' /></td>
+                    <td><Star className='star  ' /></td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td>Title2</td>
                     <td>Author3</td>
                     <td><button>Read</button></td>
-                    <td><img src="/" alt="up" /><img src="" alt="down" /></td>
-                    <td><img src="/" alt="star" /></td>
+                    <td><Thumb className='img img__up img__up--active' /><Thumb className='img img__down img__down--active' /></td>
+                    <td><Star className='star  ' /></td>
                 </tr>
                 <tr>
                     <td>1</td>
                     <td>Title1</td>
                     <td>Author1</td>
                     <td><button>Read</button></td>
-                    <td><img src="/" alt="up" /><img src="" alt="down" /></td>
-                    <td><img src="/" alt="star" /></td>
+                    <td><Thumb className='img img__up img__up--active' /><Thumb className='img img__down img__down--active' /></td>
+                    <td><Star className='star  ' /></td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td>Title2</td>
                     <td>Author3</td>
                     <td><button>Read</button></td>
-                    <td><img src="/" alt="up" /><img src="" alt="down" /></td>
-                    <td><img src="/" alt="star" /></td>
+                    <td><Thumb className='img img__up img__up--active' /><Thumb className='img img__down img__down--active' /></td>
+                    <td><Star className='star  ' /></td>
                 </tr>
                 <tr>
                     <td>1</td>
                     <td>Title1</td>
                     <td>Author1</td>
                     <td><button>Read</button></td>
-                    <td><img src="/" alt="up" /><img src="" alt="down" /></td>
-                    <td><img src="/" alt="star" /></td>
+                    <td><Thumb className='img img__up img__up--active' /><Thumb className='img img__down img__down--active' /></td>
+                    <td><Star className='star  ' /></td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td>Title2</td>
                     <td>Author3</td>
                     <td><button>Read</button></td>
-                    <td><img src="/" alt="up" /><img src="" alt="down" /></td>
-                    <td><img src="/" alt="star" /></td>
+                    <td><Thumb className='img img__up img__up--active' /><Thumb className='img img__down img__down--active' /></td>
+                    <td><Star className='star  ' /></td>
                 </tr>
             </tbody>
         </table>
